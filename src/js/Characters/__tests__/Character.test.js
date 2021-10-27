395b1757451a5dee511f32cf55a30bafe7fa85d8
Andrey.Sosnin <woodogul@yandex.ru>
2021-10-27 11:26:28 +0300

    Вынес поля attack и defence из базового класса

--- src/js/Characters/__tests__/Character.test.js
+++ src/js/Characters/__tests__/Character.test.js
@@ -1,8 +1,9 @@
 import { Character } from '../Character';
+import {Bowman} from "../Bowman";
 
 test('Валидация имени персонажа меньше 2 символов', () => {
   function createCharacter() {
-    return new Character('A', 'Character');
+    return new Bowman('A');
   }
 
   expect(createCharacter).toThrowError('name должна быть в диапазоне от 2 до 10 символов');
@@ -10,16 +11,16 @@ test('Валидация имени персонажа меньше 2 симво
 
 test('Валидация имени персонажа больше 10 символов', () => {
   function createCharacter() {
-    return new Character('Очень длинное имя персонажа', 'Character');
+    return new Bowman('Очень длинное имя персонажа');
   }
 
   expect(createCharacter).toThrowError('name должна быть в диапазоне от 2 до 10 символов');
 });
 
-test('Создание персонажа', () => {
-  const persona = new Character('Some Name', 'Character');
-  const expected = {
-    name: 'Some Name', type: 'Character', health: 100, level: 1, attack: 0, defence: 0,
-  };
-  expect(persona).toEqual(expected);
+test('Создание персонажа базового класса', () => {
+  function createCharacter() {
+    return new Character('Some Name', 'Character');
+  }
+
+  expect(createCharacter).toThrowError('Абстрактный класс не может быть создан');
 });
